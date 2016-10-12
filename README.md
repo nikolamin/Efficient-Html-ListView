5b0b865b17964273a98aad74e77eb7ab4ad23df5
Nikola Minoski <minoskinikola@gmail.com>
2016-10-12 18:48:31 +0200

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
 # Efficient-Html-ListView
 Efficiency create view elements and manage dom to render only visible elements. View Holder pattern for populating the views.
+Pool for reusing dom elements.
+You can list millions of items without performance issues.
+
+## Installation
+<script src="src/listadapter.js"></script>
+
+## Usage
+Create your html with one div as container of the items, and one child inside it as view template (currently only one type of view is supported).
+Note: Child view must support 'positon: absolute;' displaying.
+
+In your script create an instance of ListAdapter class.
+```
+var myList = ListAdapter("#list", adapter);
+```
+
+First paramter is querySelector for the list container, it can also be dom element.
+Second parameter is your adapter implemention to create an populate items.
+The adapter should be an object with following functions as property:
+onItemCreate (optional) : function(item)
+	This callback is invoked each time new item is created, so its recomended here to make references to the elements you want to change later.
+	Paramter 'item' is a view-holder, it have 'el' property which is a reference to the dom element, and 'q' property which is a shortcut-reference to the query selector of 'el', you can attach other properties (references to html elements of the view) to use them later in 'onItemLoad'.
+	Example:
+```
+	onItemCreate: function(item) {
+		item.title = item.q("h2");
+		item.description = item.q("h3");
+		item.image = item.q("img");
+	}
+```
+onItemLoad : function(item, index, itemData)
+	This callback is invoked each time item is placed to the document and will become visible, so here you should populate it with the information you want to show.
+	Example: 
+```
+	onItemLoad: function(item, index, itemData) {
+		item.title.innerText = itemData.title;
+		item.description.innerText = itemData.description;
+		item.image.src = "https://www.w3.org/html/logo/downloads/HTML5_Logo_512.png";
+	}
+```
+
+Now give the data you want to render in the list:
+```
+	myList.setItems([
+		{ title: "First Item", description: "First item description" },
+		{ title: "Second Item", description: "Second item description" },
+		{ title: "Thrid Item", description: "Thrid item description" },
+		{ title: "4th Item", description: "4th item description" }
+	]);
+```
+
+You can banchmark by telling only how many items you want to have
+```
+	myList.setItemsCount(1000000);
+```
+and populate them dynamicly:
+```
+var myList = new ListAdapter("#list", {
+	onItemCreate: function(item) {
+		item.title = item.q("h2");
+		item.description = item.q("h3");
+		item.image = item.q("img");
+	},
+	onItemLoad: function(item, index, itemData) {
+		item.title.innerText = "This is item " + (index+1);
+		item.description.innerText = "This is description for item " + (index+1);
+		item.image.src = "https://www.w3.org/html/logo/downloads/HTML5_Logo_512.png";
+	}
+});
+```
+
+## Demo
+Below you can find the demo code.
+Or you can see the demo project here: list adapter demo 
+
+## Advanced
+To avoid some flickerings when scrolling very fast, the ListAdapter loads extra views off-screen.
+By default they are 3 from top and 3 from bottom, you can configure this by changing value of `myList.offscreenItems`
---
+++ src/listadapter.js
@@ -0,0 +1,250 @@
+HTMLElement = typeof(HTMLElement) != 'undefiend' ? HTMLElement : Element;
+HTMLElement.prototype.prependChild = function(element) {
+    if (this.firstChild) {
+        return this.insertBefore(element, this.firstChild);
+    } else {
+        return this.appendChild(element);
+    }
+};
+HTMLDocument.prototype.getHeight = function() {
+	return document.body.clientHeight;
+}
+HTMLElement.prototype.getHeight = function() {
+	return this.clientHeight;
+}
+HTMLDocument.prototype.getScrollTop = HTMLElement.prototype.getScrollTop = function() {
+	var doc = document.documentElement;
+	return (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+}
+HTMLDocument.prototype.getScrollLeft = HTMLElement.prototype.getScrollLeft = function() {
+	var doc = document.documentElement;
+	return (window.pageXOffset || doc.scrollLeft) - (doc.clientLeft || 0);
+}
+
+HTMLElement.prototype.scrollParent = function() {
+	var regex = /(auto|scroll)/;
+	var parents = function (node, ps) {
+		if (node.parentNode === null) { return ps; }
+			return parents(node.parentNode, ps.concat([node]));
+	};
+	var style = function (node, prop) {
+		return getComputedStyle(node, null).getPropertyValue(prop);
+	};
+	var overflow = function (node) {
+		return style(node, "overflow") + style(node, "overflow-y") + style(node, "overflow-x");
+	};
+	var scroll = function (node) {
+		return regex.test(overflow(node));
+	};
+	var scrollParent = function () {
+		if (!(this instanceof HTMLElement)) {
+			return ;
+		}
+		var ps = parents(this.parentNode, []);
+		for (var i = 0; i < ps.length; i += 1) {
+			if (scroll(ps[i])) {
+				return ps[i];
+			}
+		}
+		return document;
+	};
+	return scrollParent;
+}();
+
+var ListAdapter = function(containerEl, adapter) {
+	if(typeof(containerEl) == "string") {
+		containerEl = document.querySelector(containerEl);
+	}
+	this.containerEl = containerEl;
+	this.itemCreateHandler = (adapter && adapter.onItemCreate) || function(){};
+	this.itemLoadHandler = (adapter && adapter.onItemLoad) || function(){};
+
+	this.itemsCount = 0;
+	this.offscreenItems = 3;
+
+	this.itemsPool = new Array();
+
+	if(!containerEl || containerEl === null)
+		throw new "List container not specified.";
+
+	if(containerEl.childElementCount == 0)
+		throw new "List container must have at least 1 element as a template.";
+
+	var item = containerEl.children[0];
+	this.itemHeight = item.offsetHeight;
+
+	this.lists = {};
+	var lists = item.querySelectorAll('list');
+	for (var i = 0; i < lists.length; i++) {
+		var list = lists[i];
+		var key = list.attributes["data-key"].value;
+		this.lists[key] = list.innerHTML;
+		list.parentNode.replaceChild("{{" + key + "}}", list);
+	};
+	item.style.position = "absolute";
+	this.itemHtml = item.outerHTML;
+
+	var item = this.createBaseViewHolder(item);
+	this.itemCreateHandler(item);
+	this.recycleItem(item.el);
+
+	this.scrollParentEl = this.containerEl.scrollParent();
+	this.scrollParentEl.addEventListener("scroll", this.postUpdate.bind(this), false);
+	window.addEventListener("resize", this.postUpdate.bind(this));
+};
+
+ListAdapter.prototype.destroy = function() {
+	this.scrollParentEl.removeEventListener("scroll", this.postUpdate.bind(this));
+	window.removeEventListener("resize", this.postUpdate.bind(this));
+	this.itemsPool = undefined;
+	this.items = undefined;
+	this.itemHtml = undefined;
+	this.containerEl = undefined;
+}
+
+
+ListAdapter.prototype.postUpdate = function() {
+	if(!this.scrollLock) {
+    	this.scrollLock = true;
+		window.requestAnimationFrame(this.updateItems.bind(this));
+	}
+}
+
+ListAdapter.prototype.setItems = function(items, offset) {
+	if(offset == undefined) {
+		this.itemsCount = items.length;
+		this.items = items;	
+	} else {
+		if((offset + items.length) < this.itemsCount) {
+			this.itemsCount = offset + items.length;
+		}
+		for (var i = 0; i < items.length; i++) {
+			this.items[i + offset] = items[i];
+		}
+	}
+	this.containerEl.style.height = this.itemsCount * this.itemHeight;
+
+	this.updateItems();
+	this.invalidate();
+};
+
+ListAdapter.prototype.setItemsCount = function(num) {
+	this.itemsCount = num;
+	this.items = new Array(num);
+
+	this.containerEl.style.height = this.itemsCount * this.itemHeight;
+	
+	this.updateItems();
+	this.invalidate();
+}
+
+ListAdapter.prototype.createBaseViewHolder = function(el) {
+	var vh = { el: el, q: el.querySelector.bind(el) };
+	el.viewHolder = vh;
+	return vh;
+}
+ListAdapter.prototype.getItemEl = function(index) {
+	var item = this.itemsPool.pop();
+	if(item == undefined) {
+		var div = document.createElement('div');
+		div.innerHTML = this.itemHtml;
+		item = this.createBaseViewHolder(div.firstChild);
+		this.itemCreateHandler(item);
+	}
+
+	this.itemLoadHandler(item, index, this.items[index]);
+
+	return item.el;
+};
+
+ListAdapter.prototype.recycleItem = function(el) {
+	this.itemsPool.push(el.viewHolder);
+	el.remove();
+};
+
+ListAdapter.prototype.updateItems = function() {
+	this.scrollLock = true;
+	var list = this.containerEl;
+	var scroll = this.scrollParentEl;
+	var itemHeight = this.itemHeight;
+	var itemHtml = this.itemHtml;
+
+	var scrollOffset = scroll.getScrollTop();
+	var topOffscreen = scrollOffset - (this.offscreenItems  * this.itemHeight);
+	var bottomOffscreen = scroll.getHeight() + (this.offscreenItems * this.itemHeight) + scrollOffset;
+
+	while(this.firstEl) {
+		var first = this.firstEl;
+		if((first.offsetTop + first.offsetHeight) < topOffscreen) {
+			this.recycleItem(first);
+			if(list.childElementCount > 0) {
+				this.firstEl = list.children[0];
+			} else {
+				this.firstEl = this.lastEl = false;
+			}
+		} else {
+			break;
+		}
+	}
+	while(this.lastEl) {
+		var last = this.lastEl;
+		if(last.offsetTop > bottomOffscreen) {
+			this.recycleItem(last);
+			if(list.childElementCount > 0) {
+				this.lastEl = list.children[list.childElementCount - 1];
+			} else {
+				this.firstEl = this.lastEl = false;
+			}
+		} else {
+			break;
+		}
+	}
+
+	while(this.firstEl && this.firstEl.offsetTop > topOffscreen) {
+		var index = this.firstEl.position - 1;
+		if(index >= 0) {
+			var el = this.getItemEl(index);
+			el.position = index;
+			el.style.top = index * itemHeight;
+			list.prependChild(el);
+			this.firstEl = el;
+			if(!this.lastEl) this.lastEl = this.firstEl;
+		} else {
+			break;
+		}
+	}
+
+	while(!this.lastEl || (this.lastEl.offsetTop + this.lastEl.offsetHeight) < bottomOffscreen) {
+		var index = 0;
+		if(this.lastEl) {
+			index = this.lastEl.position + 1;	
+		} else if(this.itemsCount > 0) {
+			index = Math.max(Math.floor(scrollOffset / itemHeight), 0);
+		} else {
+			break;
+		}
+		if(index >= 0 && index < this.itemsCount) {
+			var el = this.getItemEl(index);
+			el.position = index;
+			el.style.top = index * itemHeight;
+			list.appendChild(el);
+			this.lastEl = el;
+			if(!this.firstEl) this.firstEl = this.lastEl;
+		} else {
+			break;
+		}
+	}
+	this.scrollLock = false;
+}
+
+ListAdapter.prototype.invalidate = function() {
+	var list = this.containerEl;
+	var itemHandler = this.itemHandler;
+
+	for (var i = 0; i < list.childElementCount; i++) {
+		var item = list.children[i];
+		var index = item.position;
+		this.itemLoadHandler(item.viewHolder, index, this.items[index]);
+	};
+}
+
